a8b4f8f598b6e97f1c20350bdce9d720e38ba7b1
mbuyco <mbuyco@pop-os.localdomain>
2023-01-06 01:32:04 +0800

    feat: add scripts for creating/deleting db

---
+++ scripts/migrate.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(__DIR__ . '/../lib/constants.php');
+require_once(__DIR__ . '/../lib/services/SQLite3Driver.php');
+
+$sqlite_driver = new SQLite3Driver(DB_PATH . DB_NAME);
+$sql = file_get_contents(__DIR__ . './../data/schema.sql');
+$sqlite_driver->exec($sql);
