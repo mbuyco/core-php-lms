7bb89c1f6a953b6d95f79dcfd94ba5ccbb6ea38d
mbuyco <mbuyco@pop-os.localdomain>
2023-01-06 04:38:58 +0800

    chore: remove traces of sqlite3 driver

--- scripts/migrate.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once(__DIR__ . '/../lib/core/constants.php');
-require_once(__DIR__ . '/../lib/services/SQLite3Driver.php');
-
-$sqlite_driver = new SQLite3Driver(DB_PATH . DB_NAME);
-$sql = file_get_contents(__DIR__ . './../data/schema.sql');
-$sqlite_driver->exec($sql);
