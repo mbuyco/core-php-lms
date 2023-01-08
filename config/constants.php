36ff6818701337a7f60f94349acff55d0399dd9a
mbuyco <mbuyco@pop-os.localdomain>
2023-01-08 16:16:51 +0800

    feat: add user type constants

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,17 @@
 <?php
 
+/**
+ * This file contains all the environment variables for this application
+ */
+
+// Database configuration
 define('DB_NAME', 'core_lms');
 define('DB_HOST', 'postgres');
 define('DB_USER', 'core_lms_user');
 define('DB_PASSWORD', 'c0r3lm$');
 define('DEFAULT_PAGE_TITLE', 'Simple LMS');
+
+// User constants
+define('USER_TYPE_ADMIN', 1);
+define('USER_TYPE_TEACHER', 2);
+define('USER_TYPE_STUDENT', 3);
